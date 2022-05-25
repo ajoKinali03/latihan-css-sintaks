edc6496612431ca05783719c5c4c5ed7fa756244
ajoKinali03 <kinaliangek1205@gmail.com>
2022-05-25 16:42:20 +0700

    memberikan scrpit animasi pada style

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+//animasi test
+/*
+  cara kerja:
+    > ketika elemen box satu di tekan maka element di belkangnya akan bergeser kedalam sehingga menghilang atau tepat di belakang element box 1
+*/
+
+const box1 = document.getElementsByClassName('box1')[0];
+box1.addEventListener('click', function(){
+  const box2 = document.getElementsByClassName('box2')[0];
+  const box3 = document.getElementsByClassName('box3')[0];
+  box2.style.top = '100px';
+  box2.style.top = '100px';
+  box3.style.top = '200px';
+  box3.style.top = '100px';
+  
+  
+});
