8074ebff82f0d53ba4a9778def2b91a91e44e497
kinaliangek1205@gami.com <kinaliangek1205@gami.com>
2022-06-11 12:06:30 +0700

    perubahan line code pada js dan css( membuat properti order dan merubah dan mengatur posisi dari conten)

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-let cntr = document.getElementsByClassName('container')[0];
-let box2 = document.getElementsByClassName('box2')[0];
-let n = 0;
-cntr.addEventListener('click', function(){
-    n++;
-    if(n == 1){
-        box2.style.animationPlayState = "paused";
-    };
-    if(n == 2){
-        box2.style.animationPlayState = "running";
-    };
-    if(n >= 2){
-        n *= 0;
-    };
-});
+setInterval(function(){
+  const parentContr = document.getElementsByClassName('parent-container')[0];
+  
+  parentContr.style.height = window.innerHeight + 'px';
+}, 50);
+
